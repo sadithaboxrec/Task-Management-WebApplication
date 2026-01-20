141e452236491cca59a1377c2895ad58e918b522
Saditha Fernando <sadithafernandods2002@gmail.com>
2026-01-20 22:16:06 +0530

    Footer and Navbar

---
+++ Task-App-Frontend/src/app/layout/navbar/navbar.ts
@@ -0,0 +1,35 @@
+import { Component,HostListener  } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Router, RouterLink} from '@angular/router';
+import {Api} from '../../service/api';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule,RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+
+  constructor(private readonly api:Api, private router:Router) {
+  }
+
+  get isAuthenticated():boolean{
+    return this.api.isAuthenticated();
+  }
+
+
+  handleLogout():void{
+    const islogout = window.confirm("Do  you want to logout?");
+
+    if (islogout) {
+      this.api.logout();
+      this.router.navigate(['/login'])
+    }
+
+  }
+
+
+
+
+}
